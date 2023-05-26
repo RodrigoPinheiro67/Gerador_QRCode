faf62d8013ac2f6069233fb4352344afffe52983
Rodrigo dos Santos Pinheiro <10083905@pucrs.br>
2023-05-26 17:29:32 -0300

    otimização de código - Inserção de Try Catch para captura de exceção

--- qrcode/src/main/java/org/example/QRCodeGenerator.java
+++ qrcode/src/main/java/org/example/QRCodeGenerator.java
@@ -30,24 +30,25 @@ import net.glxn.qrgen.image.ImageType;
 public class QRCodeGenerator {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite os dados para gerar o QR Code: ");
-        String inputData = scanner.nextLine();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Digite os dados para gerar o QR Code: ");
+            String inputData = scanner.nextLine();
 
-        try {
-            ByteArrayOutputStream out = QRCode.from(inputData).to(ImageType.PNG).stream();
+            try {
+                ByteArrayOutputStream out = QRCode.from(inputData).to(ImageType.PNG).stream();
 
-            System.out.print("Digite o nome do arquivo para salvar o QR Code (exemplo: qrcode.png): ");
-            String fileName = scanner.nextLine();
+                System.out.print("Digite o nome do arquivo para salvar o QR Code (exemplo: qrcode.png): ");
+                String fileName = scanner.nextLine();
 
-            FileOutputStream fos = new FileOutputStream(fileName);
-            fos.write(out.toByteArray());
-            fos.flush();
-            fos.close();
+                FileOutputStream fos = new FileOutputStream(fileName);
+                fos.write(out.toByteArray());
+                fos.flush();
+                fos.close();
 
-            System.out.println("QR Code gerado e salvo com sucesso!");
-        } catch (Exception e) {
-            System.err.println("Ocorreu um erro ao gerar o QR Code: " + e.getMessage());
+                System.out.println("QR Code gerado e salvo com sucesso!");
+            } catch (Exception e) {
+                System.err.println("Ocorreu um erro ao gerar o QR Code: " + e.getMessage());
+            }
         }
     }
 }
